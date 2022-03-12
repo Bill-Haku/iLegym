15fe5d169010240f2e53516be98c463c1be0957e
Bill-Haku <battleandwar@hotmail.com>
2022-03-12 18:33:00 +0800

    Initial Commit

---
+++ Shared/iLegymApp.swift
@@ -0,0 +1,17 @@
+//
+//  iLegymApp.swift
+//  Shared
+//
+//  Created by Bill Haku on 2022/3/12.
+//
+
+import SwiftUI
+
+@main
+struct iLegymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
